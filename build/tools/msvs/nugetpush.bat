cbac6432e8a1dfedc2a8c8ea278deb632dea0d9c
Wu, Ganhao <wuganhao@hotmail.com>
2020-01-13 22:21:17 -0800

    Create nuget package.

--- build/tools/msvs/nugetpush.bat
+++ build/tools/msvs/nugetpush.bat
@@ -1,2 +1,2 @@
-call getversion.bat
+call %~dp0\getversion.bat
 nuget pack %~dp0\wxWidgets.nuspec -OutputFileNamesWithoutVersion -properties id=wxWidgets.VC160.Shared --properties version=%wxMAJOR_VERSION%.%wxMINOR_VERSION%.%wxRELEASE_NUMBER%.%BUILD_NUMBER% --properties dllpath=lib\vc160_x64_dll $*
