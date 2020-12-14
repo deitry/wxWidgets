7fc6e5e33ad085b831dce9a808b23e77b9dc73a8
Wu, Ganhao <wuganhao@hotmail.com>
2020-12-14 18:11:14 +0800

    Update nuget package id to wxWidgets.

--- build/tools/msvs/nugetpush.bat
+++ build/tools/msvs/nugetpush.bat
@@ -1,2 +1,2 @@
 call %~dp0\getversion.bat
-nuget pack %~dp0\wxWidgets.nuspec -OutputFileNamesWithoutVersion -properties id=wxWidgets.VC160.Shared -properties version=%wxMAJOR_VERSION%.%wxMINOR_VERSION%.%wxRELEASE_NUMBER%.%BUILD_NUMBER% -properties dllpath=%~dp0\..\..\..\lib\vc160_x64_dll %*
+nuget pack %~dp0\wxWidgets.nuspec -OutputFileNamesWithoutVersion -properties id=wxWidgets -properties version=%wxMAJOR_VERSION%.%wxMINOR_VERSION%.%wxRELEASE_NUMBER%.%BUILD_NUMBER% -properties dllpath=%~dp0\..\..\..\lib\vc160_x64_dll %*
